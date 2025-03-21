44d10d88d7d931ab4659518df0f2e1426a6016ff
gabrielvedova <bielvedova@gmail.com>
2025-03-21 20:12:50 -0300

    Add (Historia_Interativa): implement interactive scenario input

---
+++ Historia_Interativa/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+scenario = input("Qual o cenário?")
+main_character = input("Qual o personagem principal?")
+what_find = input("O que o personagem encontrou?")
+where_it = input("Onde estava a coisa que ele encontrou?")
+
+print(f'{main_character} estava no(a) {scenario} e encontrou {what_find} no(a) {where_it}.')
